a577dfe939f341295cd3e107a5f5cedec8a66817
Jonathan Hoyt <jonmagic@gmail.com>
2023-11-09 16:28:46 -0800

    Fix circular reference error

--- packages/open-truss/src/utils/yaml.ts
+++ packages/open-truss/src/utils/yaml.ts
@@ -1,7 +1,7 @@
 import yaml from 'yaml'
 
-type YamlObject = Record<string, YamlType>
-type YamlType = null | number | string | boolean | YamlObject | YamlType[]
+interface YamlObject extends Record<string, YamlType> {}
+type YamlType = null | number | string | boolean | YamlType[] | YamlObject
 
 export function parseYaml(yamlString: string): YamlType {
   return yaml.parse(yamlString)
