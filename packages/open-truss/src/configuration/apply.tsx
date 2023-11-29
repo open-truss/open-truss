959bd742213959002ef968ede06baed028f58870
Harrison Touw <hktouw@gmail.com>
2023-11-29 14:51:07 -0800

    Clean up code

--- packages/open-truss/src/configuration/apply.tsx
+++ packages/open-truss/src/configuration/apply.tsx
@@ -3,17 +3,13 @@ import type React from 'react'
 import type { BaseOpenTrussComponentV1, WorkflowV1 } from './engine-v1'
 import { engineV1 } from './engine-v1'
 
-export type RenderingEngine = () => ReactTree
-
 export interface WorkflowSpec {
   workflow: WorkflowV1 // | WorkflowV2
 }
-
 type BaseOpenTrussComponents = BaseOpenTrussComponentV1 // |BaseOpenTrussComponentV2
-type COMPONENT = React.FC<BaseOpenTrussComponents>
-export type COMPONENTS = Record<string, COMPONENT>
-
+export type COMPONENTS = Record<string, React.FC<BaseOpenTrussComponents>>
 export type ReactTree = Array<React.JSX.Element | ReactTree>
+export type RenderingEngine = () => ReactTree
 
 type ConfigurationFunction = (config: YamlObject, data: YamlType) => ReactTree
 export function applyConfiguration(COMPONENTS: COMPONENTS): ConfigurationFunction {
