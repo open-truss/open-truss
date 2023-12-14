68731304231e02d1e52bfb1642f1cd303f14b018
Harrison Touw <hktouw@gmail.com>
2023-12-14 13:38:44 -0800

    Temp delete changes, resetting back to main

--- demo-app/src/app/playground/page.tsx
+++ demo-app/src/app/playground/page.tsx
@@ -1,14 +1,12 @@
 import { parseYaml, applyConfiguration } from '@open-truss/open-truss'
-import * as COMPONENTS from '@open-truss-components'
+import * as COMPONENTS from '@/open-truss-components'
 const configurationFunction = applyConfiguration(COMPONENTS)
 
 async function PlaygroundPage(): Promise<JSX.Element> {
   const parsedConfig = parseYaml(config)
   const renderedComponents = configurationFunction(parsedConfig, {})
 
-  return <>
-    {renderedComponents}
-  </>
+  return <>{renderedComponents}</>
 }
 
 export default PlaygroundPage
@@ -18,14 +16,14 @@ workflow:
   version: 1
   frames:
     - frame:
-      data: Foo (component from application)
+      data: foo
       view:
         component: Foo
         props:
           color: blue
       frames:
         - frame:
-          data: OTBar (component from OT package)
+          data: bar
           view:
             component: OTBar
             props:
