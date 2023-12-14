aea951625f45c347b3c12cebc065955fc97b8e28
Harrison Touw <hktouw@gmail.com>
2023-12-13 16:02:10 -0800

    Fix linter

--- packages/open-truss/nextjs/src/app/page.tsx
+++ packages/open-truss/nextjs/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
