9b258938fb6b63d16af1c3852745d348a5d35401
Jonathan Hoyt <jonmagic@gmail.com>
2023-12-17 13:51:57 -0800

    Temporarily update ot/playground to execute trino query and display results

--- demo-app/src/app/ot/playground/page.tsx
+++ demo-app/src/app/ot/playground/page.tsx
@@ -1,12 +1,41 @@
-import { parseYaml, applyConfiguration } from '@open-truss/open-truss'
+import {
+  parseYaml,
+  applyConfiguration,
+  type UqiMetadata,
+} from '@open-truss/open-truss'
 import * as COMPONENTS from '@open-truss-components'
+import createTrinoUqiClient from '@/lib/trino-uqi-client'
+
 const configurationFunction = applyConfiguration(COMPONENTS)
 
 async function PlaygroundPage(): Promise<JSX.Element> {
+  const trinoUqiClient = await createTrinoUqiClient({
+    auth: 'trino',
+    server: 'http://trino.orb.local:8080',
+  })
+  const queryIterator = await trinoUqiClient.query(
+    'select * from  tpch.sf1.customer limit 10',
+  )
+  const rows = []
+  let metadata: UqiMetadata = {
+    columns: [],
+  }
+  for await (const { row, metadata: m } of queryIterator) {
+    rows.push(row)
+    if (metadata.columns.length === 0) {
+      metadata = m
+    }
+  }
   const parsedConfig = parseYaml(config)
   const renderedComponents = configurationFunction(parsedConfig, {})
 
-  return <>{renderedComponents}</>
+  return (
+    <>
+      {renderedComponents}
+      <p>{JSON.stringify(rows)}</p>
+      <p>{JSON.stringify(metadata)}</p>
+    </>
+  )
 }
 
 export default PlaygroundPage
