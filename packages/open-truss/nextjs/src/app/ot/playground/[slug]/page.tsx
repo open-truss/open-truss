5b41992800e3a804478896ae94c2bf9bb92d3522
Harrison Touw <hktouw@gmail.com>
2023-12-19 14:14:48 +0900

    Change rendering engine to look for config files at src/open-truss/configs. For now we'll store configs in the file system.

---
+++ packages/open-truss/nextjs/src/app/ot/playground/[slug]/page.tsx
@@ -0,0 +1,36 @@
+import { promises as fs } from 'fs'
+import { parseYaml, applyConfiguration } from '@open-truss/open-truss'
+import * as COMPONENTS from '@/open-truss/components'
+import path from 'path'
+
+const configurationFunction = applyConfiguration(COMPONENTS)
+
+interface PlaygroundPage {
+  params: {
+    slug: string
+  }
+}
+
+export default async function Page({
+  params: { slug },
+}: PlaygroundPage): Promise<JSX.Element> {
+  let config
+  try {
+    const sanitizedSlug = path.basename(slug)
+    config = await fs.readFile(
+      `./src/open-truss/configs/${sanitizedSlug}.yaml`,
+      'utf-8',
+    )
+  } catch (err) {
+    if ((err as NodeJS.ErrnoException).code === 'ENOENT') {
+      return <>404 - File Not Found</>
+    } else {
+      throw err
+    }
+  }
+
+  const parsedConfig = parseYaml(config)
+  const renderedComponents = configurationFunction(parsedConfig, {})
+
+  return <>{renderedComponents}</>
+}
