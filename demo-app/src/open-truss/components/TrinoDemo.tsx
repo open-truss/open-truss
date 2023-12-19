fb5fe620d405e3d4d7a77f18b685b2b50911ab64
Jonathan Hoyt <jonmagic@gmail.com>
2023-12-19 11:17:35 -0800

    Wire up TrinoDemo (not working)

---
+++ demo-app/src/open-truss/components/TrinoDemo.tsx
@@ -0,0 +1,42 @@
+import {
+  type BaseOpenTrussComponentV1,
+  type UqiMetadata,
+} from '@open-truss/open-truss'
+import createTrinoUqiClient from '@/lib/trino-uqi-client'
+
+export default async function TrinoDemo(
+  props: BaseOpenTrussComponentV1,
+): Promise<JSX.Element> {
+  const trinoUqiClient = await createTrinoUqiClient({
+    auth: 'trino',
+    server: 'http://trino.orb.local:8080',
+  })
+  const queryIterator = await trinoUqiClient.query(props.data.query)
+  const rows = []
+  let metadata: UqiMetadata = {
+    columns: [],
+  }
+  for await (const { row, metadata: m } of queryIterator) {
+    rows.push(row)
+    if (metadata.columns.length === 0) {
+      metadata = m
+    }
+  }
+
+  return (
+    <>
+      <h1>Trino Demo</h1>
+      <h2>Rows</h2>
+      {rows.map((row, i) => (
+        <div key={i}>
+          {Object.entries(row).map(([key, value]) => (
+            <div key={key}>
+              <strong>{key}</strong>: {String(value)}
+            </div>
+          ))}
+          <hr />
+        </div>
+      ))}
+    </>
+  )
+}
