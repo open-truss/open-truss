95779b77653653c800c31271ec538dd6f8f215e7
Keith McHugh <keith.mchugh@gmail.com>
2023-12-24 15:55:09 -0500

    Consolidate component getting and fixup types

--- demo-app/src/app/ot/rsc-playground/[slug]/page.tsx
+++ demo-app/src/app/ot/rsc-playground/[slug]/page.tsx
@@ -1,12 +1,6 @@
 import path from 'path'
 import RenderFromFile from '@/components/RenderFromFile'
 
-// TODO: Set COMPONENT_INDEX in application config and OT loads it?
-import * as COMPONENTS from '@/open-truss/components'
-
-// TODO: Get this path from application config and only need to pass in filename?
-const CONFIG_DIR = './src/open-truss/configs/'
-
 export default function Page({
   params: { slug },
 }: {
@@ -19,10 +13,7 @@ export default function Page({
   return (
     <>
       <h1>{sanitizedSlug}</h1>
-      <RenderFromFile
-        components={COMPONENTS}
-        path={`${CONFIG_DIR}${sanitizedSlug}.yaml`}
-      />
+      <RenderFromFile configName={sanitizedSlug} />
     </>
   )
 }
--- demo-app/src/components/RenderFromEndpoint.tsx
+++ demo-app/src/components/RenderFromEndpoint.tsx
@@ -5,14 +5,20 @@ import {
   type COMPONENTS,
 } from '@open-truss/open-truss'
 
+// TODO: Set COMPONENT_INDEX in application config and OT loads it?
+import * as _components from '@/open-truss/components'
+const components = _components as unknown as COMPONENTS
+
+// TODO: Get this path from application config and only need to pass in filename?
+const CONFIG_API = '/ot/api/configs/'
+
 export default function RenderFromEndpoint({
-  components = {},
-  url,
+  configName,
 }: {
-  components?: COMPONENTS
-  url: string
+  configName: string
 }): JSX.Element {
   // TODO: Use UQI's REST client once that exists?
+  const url = `${CONFIG_API}${configName}`
   const [config, setConfig] = React.useState<string | null>(null)
   const [loading, setLoading] = React.useState<boolean>(false)
   const [error, setError] = React.useState<Error | null>(null)
