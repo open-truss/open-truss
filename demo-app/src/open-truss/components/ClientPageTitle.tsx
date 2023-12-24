929b0974ce825afa945da74d1585e09cda1eb28c
Keith McHugh <keith.mchugh@gmail.com>
2023-12-24 10:51:11 -0500

    more linting

--- demo-app/src/open-truss/components/ClientPageTitle.tsx
+++ demo-app/src/open-truss/components/ClientPageTitle.tsx
@@ -1,9 +1,7 @@
 'use client'
 import { type BaseOpenTrussComponentV1Props } from '@open-truss/open-truss'
 
-function PageTitle({
-  children,
-}: BaseOpenTrussComponentV1Props): JSX.Element {
+function PageTitle({ children }: BaseOpenTrussComponentV1Props): JSX.Element {
   return <h1>{children}</h1>
 }
 
