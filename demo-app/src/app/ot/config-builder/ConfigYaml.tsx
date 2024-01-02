584f2a8dbca4ece58569334d270a910b08f53c95
Keith McHugh <keith.mchugh@gmail.com>
2024-01-02 15:49:36 -0800

    Move components into their own files

---
+++ demo-app/src/app/ot/config-builder/ConfigYaml.tsx
@@ -0,0 +1,73 @@
+import { useContext } from 'react'
+import { ConfigBuilderContext } from './config-builder-context'
+
+const FRAMES_PATH_PARTS = ['workflow:', 'frames:', '- frame:']
+
+export default function ConfigYaml({
+  config,
+}: {
+  config: string
+}): React.JSX.Element {
+  const { framesPath, setFramesPath } = useContext(ConfigBuilderContext)
+  const framesPathParts: Array<number | string> = []
+
+  return (
+    <>
+      {config
+        // clean up empty values to make things more readable
+        .replaceAll('frame: null', 'frame: ')
+        .replaceAll('frames: []', 'frames:')
+        .replaceAll('frames:', 'frames: ')
+        // read the config line by line
+        .split('\n')
+        .map((line, i) => {
+          const trimmedLine = line.trim()
+
+          // While iterating over the lines of the config, we construct what
+          // each 'frames:' line's `framePath` is. This will look something like
+          // 'workflow.frames.0.frames' where the integer is the index of the
+          // current frame in the current frames array. This index gets
+          // incremented when we encounter a '- frame:' line.
+
+          // `configLevel` is what indentation level within the YAML we are in.
+          // We divide by 2 because the config is indented 2 spaces per level.
+          // We use `configLevel` to know what `framePath` level we are updating.
+          const configLevel = line.search(/\S|$/) / 2
+          if (FRAMES_PATH_PARTS.includes(trimmedLine)) {
+            if (trimmedLine === '- frame:') {
+              if (framesPathParts[configLevel] === undefined) {
+                framesPathParts[configLevel] = 0
+              } else {
+                framesPathParts[configLevel] =
+                  Number(framesPathParts[configLevel]) + 1
+              }
+            } else {
+              framesPathParts[configLevel] = trimmedLine.replace(/:$/, '')
+            }
+          }
+
+          if (trimmedLine.startsWith('frames:')) {
+            const thisFramesPath = framesPathParts.join('.')
+            const selectedFramesPath = thisFramesPath === framesPath
+            return (
+              <pre
+                key={i}
+                style={{ color: selectedFramesPath ? 'pink' : 'black' }}
+              >
+                {line}
+                <button
+                  onClick={() => {
+                    setFramesPath(thisFramesPath)
+                  }}
+                >
+                  Nest at this level
+                </button>
+              </pre>
+            )
+          } else {
+            return <pre key={i}>{line}</pre>
+          }
+        })}
+    </>
+  )
+}
---
+++ demo-app/src/app/ot/config-builder/PropInput.tsx
@@ -0,0 +1,99 @@
+import { useEffect } from 'react'
+import {
+  type YamlType,
+  type ZodDescriptionObject,
+} from '@open-truss/open-truss'
+
+export default function PropInput({
+  name,
+  onChange,
+  type,
+  value,
+}: {
+  name: string
+  onChange: (value: YamlType) => void
+  type: ZodDescriptionObject
+  value?: string
+}): React.JSX.Element | null {
+  const defaultValue = type.defaultValue as string
+  value = value ?? defaultValue
+
+  useEffect(() => {
+    onChange(defaultValue)
+  }, [defaultValue])
+
+  if (name === 'children') {
+    return null
+  }
+
+  switch (type.type) {
+    case 'ZodUndefined':
+    case 'ZodNull':
+    case 'ZodVoid':
+      return null
+    case 'ZodNumber':
+    case 'ZodBigInt':
+      return (
+        <input
+          name={name}
+          type="number"
+          value={Number(value)}
+          onChange={(e) => {
+            onChange(Number(e.target.value))
+          }}
+        />
+      )
+    case 'ZodString':
+      return (
+        <input
+          name={name}
+          placeholder={name}
+          value={value}
+          onChange={(e) => {
+            onChange(e.target.value)
+          }}
+        />
+      )
+    case 'ZodBoolean':
+      return (
+        <div>
+          <label htmlFor={name}>{name}</label>
+          <input
+            type="checkbox"
+            id={name}
+            name={name}
+            placeholder={name}
+            checked={Boolean(value)}
+            onChange={(e) => {
+              onChange(e.target.checked)
+            }}
+          />
+        </div>
+      )
+    case 'ZodEnum':
+      if (Array.isArray(type?.shape)) {
+        const enumOptions = type.shape.map((option: string) => (
+          <div key={option}>
+            <label htmlFor={option}>{option}</label>
+            <input
+              type="radio"
+              id={option}
+              name={option}
+              value={option}
+              checked={value === option}
+              onChange={(e) => {
+                onChange(e.target.value)
+              }}
+            />
+          </div>
+        ))
+        return <>{enumOptions}</>
+      }
+  }
+
+  return (
+    <div>
+      Name: {name}. type: {JSON.stringify(type)}
+    </div>
+  )
+}
