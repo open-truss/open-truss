9cbf4c0f7fef00de808b5057ce0621108dd31789
Jonathan Hoyt <jonmagic@gmail.com>
2024-01-03 13:55:05 -0800

    Spike mysql-demo source

--- demo-app/src/lib/uqi-sources.ts
+++ demo-app/src/lib/uqi-sources.ts
@@ -1,12 +1,22 @@
 import createTrinoUqiClient from '@/lib/trino-uqi-client'
 
 const sources = {
+  // setup local trino with this:
+  // docker run --name trino -d -p 8080:8080 trinodb/trino
   'trino-demo': {
     config: {
       auth: 'trino',
       server: String(process.env.TRINO_DEMO_SERVER),
     },
     createClient: createTrinoUqiClient,
+  },
+  'mysql-demo': {
+    // setup local mysql with this:
+    // docker run --name mysql8 -e MYSQL_ROOT_PASSWORD="passw0rd" -e MYSQL_USER=admin -e MYSQL_PASSWORD="passw0rd" -d mysql:8
+    config: {
+      // ...
+    },
+    // createClient: createMysqlUqiClient,
   },
 }
 
