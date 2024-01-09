d49e58c9516c42762fb4c59f6020cc8714253f19
Harrison Touw <hktouw@gmail.com>
2024-01-09 12:33:21 +0900

    Add jsx to webpack loader for demo app

--- demo-app/next.config.js
+++ demo-app/next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          ...config.module.rules,
+          {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: [defaultLoaders.babel],
+          },
+        ],
+      },
+    };
+  },
+};
