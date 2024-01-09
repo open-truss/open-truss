51ca7a8929039b5c4f97dc31a48bd8410d2a43d9
Harrison Touw <hktouw@gmail.com>
2024-01-09 12:34:44 +0900

    Update component to use next 13 router api instead of 14

--- demo-app/src/pages/ot/playground/[slug]/index.tsx
+++ demo-app/src/pages/ot/playground/[slug]/index.tsx
@@ -1,14 +1,14 @@
 'use client'
 import path from 'path'
+import { useRouter } from 'next/router'
 import RenderFromEndpoint from '@/components/RenderFromEndpoint'
 
-export default function Page({
-  params: { slug },
-}: {
-  params: {
-    slug: string
+export default function Page(): JSX.Element {
+  const router = useRouter()
+  const { slug } = router.query
+  if (slug === undefined || Array.isArray(slug)) {
+    return <>not found</>
   }
-}): JSX.Element {
   const sanitizedSlug = path.basename(slug)
 
   return (
