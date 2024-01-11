26466baed8fa36be3b198b571eee5e1ec513a278
Harrison Touw <hktouw@gmail.com>
2024-01-11 16:33:48 +0900

    Add OTAvailableWorkflowsFromEndpoint

--- packages/open-truss/src/components/index.ts
+++ packages/open-truss/src/components/index.ts
@@ -1,3 +1,9 @@
 import * as _OTExampleComponent from './OTExampleComponent'
+import * as _OTAvailableWorkflowsFromEndpoint from './OTAvailableWorkflowsFromEndpoint'
 export const OTExampleComponent = _OTExampleComponent
-export const OT_COMPONENTS = { OTExampleComponent }
+export const OTAvailableWorkflowsFromEndpoint =
+  _OTAvailableWorkflowsFromEndpoint
+export const OT_COMPONENTS = {
+  OTAvailableWorkflowsFromEndpoint,
+  OTExampleComponent,
+}
