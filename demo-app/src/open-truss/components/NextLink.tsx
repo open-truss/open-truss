7c43ba7bc7b69f9236ea2bf839a8d3f959e0c3b2
Harrison Touw <hktouw@gmail.com>
2024-01-11 15:46:02 +0900

    Add NextLink component to the demo app
    This will be injected as a prop / dependency into
    AvailableWorkflowsFromEndpoint at runtime by the config engine

---
+++ demo-app/src/open-truss/components/NextLink.tsx
@@ -0,0 +1,16 @@
+import {
+  withChildren,
+  BaseOpenTrussComponentV1PropsShape,
+} from '@open-truss/open-truss'
+import Link from 'next/link'
+import { z } from 'zod'
+
+export const Props = withChildren(BaseOpenTrussComponentV1PropsShape).extend({
+  to: z.string(),
+})
+
+export default function NextLink(props: z.infer<typeof Props>): JSX.Element {
+  // z.string is type string | undefined and Link doesn't like that
+  // need to figure out how to get zod to not have types with undefined
+  return <Link href={props.to || '/'}>{props.children}</Link>
+}
--- demo-app/src/open-truss/components/index.ts
+++ demo-app/src/open-truss/components/index.ts
@@ -1,3 +1,4 @@
 export * as ClientPageTitle from './ClientPageTitle'
+export * as NextLink from './NextLink'
 export * as AvailableWorkflowsFromEndpoint from './AvailableWorkflowsFromEndpoint'
 export * as YourAppExampleComponent from './YourAppExampleComponent'
