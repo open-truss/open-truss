bbb76e91e8550a94a04ae0c2804c432b25affa5f
Harrison Touw <hktouw@gmail.com>
2024-01-16 17:07:26 +0900

    Move into render function

--- packages/open-truss/src/configuration/engine-v1.tsx
+++ packages/open-truss/src/configuration/engine-v1.tsx
@@ -9,41 +9,59 @@ export function engineV1(
   COMPONENTS: COMPONENTS,
   config: WorkflowV1,
 ): RenderingEngine {
+  const globalContext = { config, COMPONENTS }
   const renderFrames = (frames: FrameV1[]): ReactTree => {
-    return frames.map(({ view, data, frames: subFrame }, i) => {
-      const { component, props: viewProps } = view
-      const Component = getComponent(component, COMPONENTS)
-      const props = processProps({ data, config, viewProps, COMPONENTS })
-
-      if (subFrame === undefined) {
-        if (data) {
-          return <DataProvider key={i} {...props} component={Component} />
-        } else {
-          return <Component key={i} {...props} />
-        }
-      }
-
-      if (!hasChildren(Component)) {
-        throw new Error(
-          `${component} given \`frames\` but doesn't support \`children\``,
-        )
-      }
-
-      const subFrames = renderFrames(subFrame).map((child, k) => {
-        return (
-          <React.Fragment key={k}>{child as React.ReactNode}</React.Fragment>
-        )
-      })
-      const children = <>{subFrames}</>
-      return (
-        <Component key={i} {...props}>
-          {children}
-        </Component>
-      )
+    return frames.map((frame, i) => {
+      return renderFrame({ frame, globalContext, renderFrames, i })
     })
   }
 
   return () => {
     return renderFrames(config.frames)
   }
+}
+
+interface FrameContext {
+  frame: FrameV1
+  globalContext: {
+    config: WorkflowV1
+    COMPONENTS: COMPONENTS
+  }
+  renderFrames: (frames: FrameV1[]) => ReactTree
+  i: number
+}
+
+function renderFrame(frameContext: FrameContext): ReactTree | JSX.Element {
+  const {
+    frame: { view, data, frames },
+    globalContext: { COMPONENTS, config },
+    renderFrames,
+    i,
+  } = frameContext
+  const { component, props: viewProps } = view
+  const Component = getComponent(component, COMPONENTS)
+  const props = processProps({ data, config, viewProps, COMPONENTS })
+  if (frames === undefined) {
+    if (data) {
+      return <DataProvider key={i} {...props} component={Component} />
+    } else {
+      return <Component key={i} {...props} />
+    }
+  }
+
+  if (!hasChildren(Component)) {
+    throw new Error(
+      `${component} given \`frames\` but doesn't support \`children\``,
+    )
+  }
+
+  const subFrames = renderFrames(frames).map((child, k) => {
+    return <React.Fragment key={k}>{child as React.ReactNode}</React.Fragment>
+  })
+  const children = <>{subFrames}</>
+  return (
+    <Component key={i} {...props}>
+      {children}
+    </Component>
+  )
 }
