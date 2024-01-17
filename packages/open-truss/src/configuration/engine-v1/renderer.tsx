52094d82164cfaa84c65850da07267e2e72bd30b
Harrison Touw <hktouw@gmail.com>
2024-01-17 17:03:22 +0900

    Remove unnecessary conditions now that we have types

--- packages/open-truss/src/configuration/engine-v1/renderer.tsx
+++ packages/open-truss/src/configuration/engine-v1/renderer.tsx
@@ -74,19 +74,8 @@ function processProps({
   if (viewProps !== undefined) {
     for (const propName in viewProps) {
       const prop = viewProps[propName]
-      // I wonder if we should have a dedicated config validation function.
-      // That way we can ensure fields have certain properties and
-      // avoid having to do those checks in the rendering engine.
-      // e.g. if a prop name is set in the config we can enforce that the
-      // value is always an object and never have to do null / object / array
-      // checks like I'm doing below
-      if (
-        prop &&
-        typeof prop === 'object' &&
-        !Array.isArray(prop) &&
-        prop.type === 'component'
-      ) {
-        newProps[propName] = getComponent(prop.value as string, COMPONENTS)
+      if (prop.type === 'component') {
+        newProps[propName] = getComponent(prop.value, COMPONENTS)
       }
     }
   }
