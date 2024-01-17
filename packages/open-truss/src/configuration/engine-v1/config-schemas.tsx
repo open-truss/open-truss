ea9459736affa950c0a09fac63744f32b24180fa
Jonathan Hoyt <jonmagic@gmail.com>
2024-01-17 15:23:46 -0800

    wip component and config-schemas have a circular reference
    Co-authored-by: Harrison Touw <hktouw@gmail.com>

--- packages/open-truss/src/configuration/engine-v1/config-schemas.tsx
+++ packages/open-truss/src/configuration/engine-v1/config-schemas.tsx
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 import { YamlShape } from '../../utils/yaml'
+import { type OpenTrussComponentExports } from '../apply'
 import { RUNTIME_COMPONENTS } from './engine'
-import { hasChildren } from './component'
 
 // Data Schemas
 /*
@@ -108,3 +108,44 @@ export const WorkflowV1Shape = z.object({
   frames: FrameV1Shape.array(),
 })
 export type WorkflowV1 = z.infer<typeof WorkflowV1Shape>
+
+export const BaseOpenTrussComponentV1PropsShape = z.object({
+  data: DataV1Shape,
+  config: WorkflowV1Shape,
+})
+export const withChildrenV1 = (shape: z.AnyZodObject): z.AnyZodObject =>
+  shape.extend({ children: z.any().optional() })
+const ComponentWithChildrenShape = withChildrenV1(
+  BaseOpenTrussComponentV1PropsShape,
+)
+type ComponentWithChildren = z.infer<typeof ComponentWithChildrenShape>
+
+export type BaseOpenTrussComponentV1Props =
+  | z.infer<typeof BaseOpenTrussComponentV1PropsShape>
+  | ComponentWithChildren
+
+export type BaseOpenTrussComponentV1 = (
+  props: BaseOpenTrussComponentV1Props,
+) => React.JSX.Element
+
+export function hasDefaultExport(
+  component: any,
+): component is OpenTrussComponentExports {
+  return 'default' in component
+}
+
+function hasPropsExport(
+  component: any,
+): component is OpenTrussComponentExports {
+  return 'Props' in component
+}
+
+export function hasChildren(
+  component: any,
+): component is ComponentWithChildren {
+  if (hasPropsExport(component)) {
+    return 'children' in component.Props.shape
+  }
+  // Default to true for legacy component definitions
+  return true
+}
