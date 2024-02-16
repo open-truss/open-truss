b147af8da6b2bcc23b61277df98432931c6d373f
Jonathan Hoyt <jonmagic@gmail.com>
2024-02-16 13:44:51 -0800

    Update root page to use Card and tailwind styles

--- demo-app/src/pages/index.tsx
+++ demo-app/src/pages/index.tsx
@@ -1,17 +1,47 @@
 import Link from 'next/link'
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+  CardDescription,
+} from '@/components/ui/card'
 
 export default function Home(): JSX.Element {
+  const year = new Date().getFullYear()
+
   return (
     <main>
-      <h1>Model Home</h1>
-      <ul>
-        <li>
-          <Link href="/ot/playground">Client Playground</Link>
-        </li>
-        <li>
-          <Link href="/ot/config-builder">Config Builder</Link>
-        </li>
-      </ul>
+      <Card className="m-6">
+        <CardHeader>
+          <CardTitle>Model Home</CardTitle>
+          <CardDescription>an open-truss demo app</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ul className="list-disc list-inside">
+            <li>
+              <Link
+                href="/ot/playground"
+                className="text-blue-500 hover:underline"
+              >
+                Client Playground
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/ot/config-builder"
+                className="text-blue-500 hover:underline"
+              >
+                Config Builder
+              </Link>
+            </li>
+          </ul>
+        </CardContent>
+        <CardFooter>
+          <p>© {year} Open Truss</p>
+        </CardFooter>
+      </Card>
     </main>
   )
 }
