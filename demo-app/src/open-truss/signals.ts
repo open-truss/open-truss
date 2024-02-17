556ff2372c6b4d2bf7c6e032193b95fe2f7ef274
Harrison Touw <hktouw@gmail.com>
2024-02-17 15:18:19 -0800

    Add more signal types

--- demo-app/src/open-truss/signals.ts
+++ demo-app/src/open-truss/signals.ts
@@ -1,3 +1,12 @@
 import { SignalType } from '@open-truss/open-truss'
 
+// scalar types
+export const NumbersSignal = SignalType<number[]>('number[]', [])
+export const NumberSignal = SignalType<number>('number', 0)
+export const StringsSignal = SignalType<string[]>('string[]', [])
+export const StringSignal = SignalType<string>('string', '')
+export const BooleansSignal = SignalType<boolean[]>('boolean[]', [])
+export const BooleanSignal = SignalType<boolean>('boolean', false)
+
+// higher order types
 export const AccountIDsSignal = SignalType<number[]>('AccountIDs', [])
