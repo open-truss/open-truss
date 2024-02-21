acf0df37c586738bc57f0e9f6f0d5f57464ae1c7
Jonathan Hoyt <jonmagic@gmail.com>
2024-02-20 19:45:02 -0800

    Don't nest everything in <main> tag

--- demo-app/src/pages/index.tsx
+++ demo-app/src/pages/index.tsx
@@ -9,33 +9,31 @@ import {
 
 export default function Home(): JSX.Element {
   return (
-    <main>
-      <Card className="m-6">
-        <CardHeader>
-          <CardTitle>Model Home</CardTitle>
-          <CardDescription>an open-truss demo app</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <ul className="list-disc list-inside">
-            <li>
-              <Link
-                href="/ot/playground"
-                className="text-blue-500 hover:underline"
-              >
-                Client Playground
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/ot/config-builder"
-                className="text-blue-500 hover:underline"
-              >
-                Config Builder
-              </Link>
-            </li>
-          </ul>
-        </CardContent>
-      </Card>
-    </main>
+    <Card className="m-6">
+      <CardHeader>
+        <CardTitle>Model Home</CardTitle>
+        <CardDescription>an open-truss demo app</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ul className="list-disc list-inside">
+          <li>
+            <Link
+              href="/ot/playground"
+              className="text-blue-500 hover:underline"
+            >
+              Client Playground
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/ot/config-builder"
+              className="text-blue-500 hover:underline"
+            >
+              Config Builder
+            </Link>
+          </li>
+        </ul>
+      </CardContent>
+    </Card>
   )
 }
