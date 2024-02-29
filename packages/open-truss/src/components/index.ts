d69fae239b90adfbac066c40f6772e3cb6a631b3
Harrison Touw <hktouw@gmail.com>
2024-02-29 11:27:08 -0800

    Create OTUqiDataProvider component. First pass.
    Co-authored-by: Jon Hoyt <jonmagic@github.com>

--- packages/open-truss/src/components/index.ts
+++ packages/open-truss/src/components/index.ts
@@ -1,9 +1,12 @@
 import * as _OTExampleComponent from './OTExampleComponent'
 import * as _OTAvailableWorkflowsFromEndpoint from './OTAvailableWorkflowsFromEndpoint'
+import * as _OTUqiDataProvider from './OTUqiDataProvider'
 export const OTExampleComponent = _OTExampleComponent
+export const OTUqiDataProvider = _OTUqiDataProvider
 export const OTAvailableWorkflowsFromEndpoint =
   _OTAvailableWorkflowsFromEndpoint
 export const OT_COMPONENTS = {
   OTAvailableWorkflowsFromEndpoint,
   OTExampleComponent,
+  OTUqiDataProvider,
 }
