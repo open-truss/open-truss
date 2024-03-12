4c8c5513eaf8469a7e0a4a9e07d2d265ad8fa7ad
Fiona Ochs <55855284+fionaochs@users.noreply.github.com>
2024-03-12 09:52:40 -0700

    using and logging updated editor query values for monaco

--- demo-app/src/pages/ot/query-editor/index.tsx
+++ demo-app/src/pages/ot/query-editor/index.tsx
@@ -14,10 +14,12 @@ import {
 } from '@/components/ui/table'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Textarea } from '@/components/ui/textarea'
+import Editor from '@monaco-editor/react'
 import { useState } from 'react'
 
 export default function QueryEditor(): JSX.Element {
   const [query, setQuery] = useState('SELECT id, login FROM users')
+  const [editorQuery, setEditorQuery] = useState('SELECT id, login FROM users')
 
   const handleQueryChange = (
     event: React.ChangeEvent<HTMLTextAreaElement>,
@@ -28,88 +30,103 @@ export default function QueryEditor(): JSX.Element {
     alert(query)
   }
 
+  // Monaco
+  const queryChangeHandler = (value: string): void => {
+    setEditorQuery(value)
+  }
+  console.log({ editorQuery })
   return (
-    <ResizablePanelGroup direction="horizontal" className="h-full w-full">
-      <ResizablePanel>
-        <ResizablePanelGroup direction="vertical">
-          <ResizablePanel defaultSize={50} className="bg-gray-100 min-h-48">
-            <div className="p-2 h-full">
-              <Textarea
-                className="p-2 h-[calc(100%-48px)]"
-                value={query}
-                onChange={handleQueryChange}
-              />
-              <div className="flex justify-end">
-                <Button
-                  className="mt-2 bg-white text-gray-600 hover:bg-gray-200 hover:text-gray-800"
-                  onClick={handleRun}
-                >
-                  Run
-                </Button>
+    <>
+      <Editor
+        height="90vh"
+        defaultLanguage="sql"
+        defaultValue={editorQuery}
+        onChange={(value: string | undefined) => {
+          queryChangeHandler(value || '')
+        }}
+      />
+      <ResizablePanelGroup direction="horizontal" className="h-full w-full">
+        <ResizablePanel>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel defaultSize={50} className="bg-gray-100 min-h-48">
+              <div className="p-2 h-full">
+                <Textarea
+                  className="p-2 h-[calc(100%-48px)]"
+                  value={query}
+                  onChange={handleQueryChange}
+                />
+                <div className="flex justify-end">
+                  <Button
+                    className="mt-2 bg-white text-gray-600 hover:bg-gray-200 hover:text-gray-800"
+                    onClick={handleRun}
+                  >
+                    Run
+                  </Button>
+                </div>
               </div>
-            </div>
-          </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[100px]">id</TableHead>
-                  <TableHead>login</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                <TableRow>
-                  <TableCell>1</TableCell>
-                  <TableCell>hktouw</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>2</TableCell>
-                  <TableCell>jonmagic</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>3</TableCell>
-                  <TableCell>kmcq</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </ResizablePanel>
-        </ResizablePanelGroup>
-      </ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={25}>
-        <Tabs defaultValue="recent" className="m-2">
-          <TabsList>
-            <TabsTrigger value="recent">Recent</TabsTrigger>
-            <TabsTrigger value="schemas">Catalog</TabsTrigger>
-          </TabsList>
-          <TabsContent value="recent">
-            <div className="mb-2 p-2 bg-gray-100 rounded-sm text-sm">
-              SELECT id, login FROM users
-            </div>
-            <div className="mb-2 p-2 bg-gray-100 rounded-sm text-sm">
-              DESCRIBE spamurai.spamurai.user_destroys
-            </div>
-          </TabsContent>
-          <TabsContent value="schemas">
-            <div className="mb-2 p-2 bg-gray-100 rounded-sm">
-              <h3 className="font-medium">abuse_classifications</h3>
-              <ul className="text-sm">
-                <li>id: bigint</li>
-                <li>previous_classification: string</li>
-                <li>current_classification: string</li>
-              </ul>
-            </div>
-            <div className="mb-2 p-2 bg-gray-100 rounded-sm">
-              <h3 className="font-medium">users</h3>
-              <ul className="text-sm">
-                <li>id: bigint</li>
-                <li>login: string</li>
-              </ul>
-            </div>
-          </TabsContent>
-        </Tabs>
-      </ResizablePanel>
-    </ResizablePanelGroup>
+            </ResizablePanel>
+            <ResizableHandle withHandle />
+            <ResizablePanel>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[100px]">id</TableHead>
+                    <TableHead>login</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>1</TableCell>
+                    <TableCell>hktouw</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>2</TableCell>
+                    <TableCell>jonmagic</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>3</TableCell>
+                    <TableCell>kmcq</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={25}>
+          <Tabs defaultValue="recent" className="m-2">
+            <TabsList>
+              <TabsTrigger value="recent">Recent</TabsTrigger>
+              <TabsTrigger value="schemas">Catalog</TabsTrigger>
+            </TabsList>
+            <TabsContent value="recent">
+              <div className="mb-2 p-2 bg-gray-100 rounded-sm text-sm">
+                SELECT id, login FROM users
+              </div>
+              <div className="mb-2 p-2 bg-gray-100 rounded-sm text-sm">
+                DESCRIBE spamurai.spamurai.user_destroys
+              </div>
+            </TabsContent>
+            <TabsContent value="schemas">
+              <div className="mb-2 p-2 bg-gray-100 rounded-sm">
+                <h3 className="font-medium">abuse_classifications</h3>
+                <ul className="text-sm">
+                  <li>id: bigint</li>
+                  <li>previous_classification: string</li>
+                  <li>current_classification: string</li>
+                </ul>
+              </div>
+              <div className="mb-2 p-2 bg-gray-100 rounded-sm">
+                <h3 className="font-medium">users</h3>
+                <ul className="text-sm">
+                  <li>id: bigint</li>
+                  <li>login: string</li>
+                </ul>
+              </div>
+            </TabsContent>
+          </Tabs>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </>
   )
 }
