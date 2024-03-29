4f44f803499852bdd1f126a7d80a3c4751ea6438
Harrison Touw <hktouw@gmail.com>
2024-03-29 11:23:06 -0700

    Refactor OTUqiDataProvider to use signals hooks
    There was some weird interaction with react hooks
    and signals leading to failures. I think signals
    uses react hooks underneath the hood and using
    the hooks again inside of components causes weirdness.
    
    As a general rule, when we want to react to signals
    updates we should probably use the signals hooks
    useComputed useSignalEffect instead of the react
    hooks.

--- packages/open-truss/src/components/OTUqiDataProvider.tsx
+++ packages/open-truss/src/components/OTUqiDataProvider.tsx
@@ -6,7 +6,12 @@ import {
 } from '../configuration/engine-v1'
 import { parseUqiResult, type SynchronousQueryResult } from '../uqi/uqi'
 import { isObject } from '../utils/misc'
-import { StringSignal, UnknownSignal, signalValueShape } from '../signals'
+import {
+  StringSignal,
+  UnknownSignal,
+  signalValueShape,
+  useSignalEffect,
+} from '../signals'
 import { z } from 'zod'
 
 export const Props = BaseOpenTrussComponentV1PropsShape.extend({
@@ -20,10 +25,10 @@ const OTUqiDataProvider: BaseOpenTrussComponentV1<z.infer<typeof Props>> = (
   props,
 ) => {
   const { query, children, output, source } = props
-  const [queryResults, setQueryResults] =
-    React.useState<SynchronousQueryResult>()
 
-  React.useEffect(() => {
+  useSignalEffect(() => {
+    let queryResults: SynchronousQueryResult
+    if (query.value === '') return
     const fetchData = async function (): Promise<undefined> {
       const result = await fetch('/api/synchronous-uqi-query', {
         method: 'POST',
@@ -33,28 +38,30 @@ const OTUqiDataProvider: BaseOpenTrussComponentV1<z.infer<typeof Props>> = (
         body: JSON.stringify({ query, source }),
       })
       const deserialized = await result.json()
-      setQueryResults(deserialized)
+      queryResults = deserialized
     }
 
-    fetchData().catch(console.error)
-  }, [])
+    fetchData()
+      .then(() => {
+        if (queryResults === undefined) return
 
-  if (queryResults === undefined) return <></>
+        for (const signal of output || []) {
+          const shape = signalValueShape(signal)
+          const parsedResults = parseUqiResult(queryResults)
+          const defaultValue = shape.parse(undefined)
 
-  for (const signal of output || []) {
-    const shape = signalValueShape(signal)
-    const parsedResults = parseUqiResult(queryResults)
-    const defaultValue = shape.parse(undefined)
+          const result = Array.isArray(defaultValue)
+            ? parsedResults
+            : isObject(defaultValue)
+            ? parsedResults[0]
+            : parsedResults[0]?.[signal.yamlName]
 
-    const result = Array.isArray(defaultValue)
-      ? parsedResults
-      : isObject(defaultValue)
-      ? parsedResults[0]
-      : parsedResults[0]?.[signal.yamlName]
-
-    const validatedResult = shape.parse(result)
-    signal.value = validatedResult
-  }
+          const validatedResult = shape.parse(result)
+          signal.value = validatedResult
+        }
+      })
+      .catch(console.error)
+  })
 
   return children
 }
