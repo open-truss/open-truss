285a28857d6185d1c233860b9c87e6a37bf1a77e
Harrison Touw <hktouw@gmail.com>
2024-03-29 11:27:24 -0700

    Set content type to json

--- packages/open-truss/src/components/OTUqiDataProvider.tsx
+++ packages/open-truss/src/components/OTUqiDataProvider.tsx
@@ -32,9 +32,7 @@ const OTUqiDataProvider: BaseOpenTrussComponentV1<z.infer<typeof Props>> = (
     const fetchData = async function (): Promise<undefined> {
       const result = await fetch('/api/synchronous-uqi-query', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ query, source }),
       })
       const deserialized = await result.json()
