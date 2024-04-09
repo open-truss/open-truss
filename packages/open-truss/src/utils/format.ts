d8ea1493c1e1e890517eeb7a1fda261f7b81cd6a
Jonathan Hoyt <jonmagic@gmail.com>
2024-04-09 10:44:07 -0700

    Sort alphabetically

--- packages/open-truss/src/utils/format.ts
+++ packages/open-truss/src/utils/format.ts
@@ -30,9 +30,9 @@ function formatParams(params: Record<string, Param>): Record<string, string> {
 export function formatQuery(template: string, config?: Config): string {
   return sqlFormat(template, {
     language: config?.language || 'sql',
-    tabWidth: 4,
     linesBetweenQueries: 2,
-    paramTypes: { named: [':'] },
     params: config?.params ? formatParams(config.params) : undefined,
+    paramTypes: { named: [':'] },
+    tabWidth: 4,
   })
 }
