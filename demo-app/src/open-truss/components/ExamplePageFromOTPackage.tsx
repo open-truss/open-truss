fde4e11203381515235283d5655277a4a099f132
Jonathan Hoyt <jonmagic@gmail.com>
2024-05-01 11:23:43 -0700

    Update ExamplePageFromOTPackage to import config-builder.css

--- demo-app/src/open-truss/components/ExamplePageFromOTPackage.tsx
+++ demo-app/src/open-truss/components/ExamplePageFromOTPackage.tsx
@@ -1,5 +1,5 @@
 import { ExamplePage } from '@open-truss/open-truss/pages'
-import '@open-truss/open-truss/pages-css'
+import '@open-truss/open-truss/styles/config-builder.css'
 const ExamplePageFromOTPackage = ExamplePage.default
 
 export default ExamplePageFromOTPackage
