625fac7a5565868aa82a60a39f4b950e0dd40f26
Jonathan Hoyt <jonmagic@gmail.com>
2024-05-09 09:00:57 -0700

    Add src/pages/api/[...graphql].ts

---
+++ demo-app/src/pages/api/[...graphql].ts
@@ -0,0 +1,59 @@
+/* eslint-disable check-file/filename-naming-convention */
+
+import { type Context } from '@/types/context'
+import { type HelloResponse, type Resolvers } from '@/types/generated-types'
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+import { loadSchemaSync } from '@graphql-tools/load'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { createYoga } from 'graphql-yoga'
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const typeDefs = loadSchemaSync('src/types/schema.graphql', {
+  loaders: [new GraphQLFileLoader()],
+})
+
+const resolvers: Resolvers<Context> = {
+  Query: {
+    hello: () => ({ message: 'Hello World!' }),
+  },
+  Mutation: {
+    hello: (_, { input }) => {
+      return { message: `Hello ${input.name}!` }
+    },
+  },
+  Subscription: {
+    hello: {
+      subscribe: async function* (_, { names }) {
+        for (const name of names) {
+          yield { message: `Hello ${name}` }
+          await sleep(1000)
+        }
+      },
+      resolve: (payload: HelloResponse) => payload,
+    },
+  },
+}
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+})
+
+const defaultQuery = `# Welcome to GraphQL Yoga
+# Try running a query here
+query {
+  hello {
+    message
+  }
+}`
+
+const server = createYoga({
+  schema,
+  graphiql: {
+    defaultQuery,
+  },
+  graphqlEndpoint: '/api/graphql',
+})
+
+export default server
