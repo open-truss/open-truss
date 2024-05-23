9c0202ac17745f763a64713e24fa092db49b7087
Andrew Dillon <andrew-dillon@github.com>
2024-05-23 17:52:37 -0500

    Add utils/template test

---
+++ packages/open-truss/src/utils/template.test.ts
@@ -0,0 +1,26 @@
+import { StringOrTemplate, resolveStringOrTemplate, resolveTemplate } from './template'
+
+const signal = (value: any) => ({ value, toString: () => String(value) })
+
+test('resolveTemplate', () => {
+  const actual = resolveTemplate({
+    template: '/search/issues?q=repo:${repo_nwo}+is:${issue_or_pr}',
+    repo_nwo: signal('open-truss/open-truss'),
+    issue_or_pr: 'issue',
+  })
+  const expected = '/search/issues?q=repo:open-truss/open-truss+is:issue'
+  expect(expected).toBe(actual)
+})
+
+
+test('resolveStringOrTemplate', () => {
+  const actual = resolveStringOrTemplate({
+    template: '/search/issues?q=repo:${repo_nwo}+is:${issue_or_pr}',
+    repo_nwo: signal('open-truss/open-truss'),
+    issue_or_pr: 'issue',
+  })
+  const expected = '/search/issues?q=repo:open-truss/open-truss+is:issue'
+  expect(expected).toBe(actual)
+
+  expect(resolveStringOrTemplate(StringOrTemplate.parse(signal('hello')))).toBe('hello')
+})
