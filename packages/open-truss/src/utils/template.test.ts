596da2d1d11c1d2b2b8d00f483c261f729529158
Andrew Dillon <andrew-dillon@github.com>
2024-05-23 17:59:32 -0500

    Linter again

--- packages/open-truss/src/utils/template.test.ts
+++ packages/open-truss/src/utils/template.test.ts
@@ -1,5 +1,11 @@
-import { StringOrTemplate, resolveStringOrTemplate, resolveTemplate } from './template'
+/* eslint-disable no-template-curly-in-string */
+import {
+  StringOrTemplate,
+  resolveStringOrTemplate,
+  resolveTemplate,
+} from './template'
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const signal = (value: any) => ({ value, toString: () => String(value) })
 
 test('resolveTemplate', () => {
@@ -12,7 +18,6 @@ test('resolveTemplate', () => {
   expect(expected).toBe(actual)
 })
 
-
 test('resolveStringOrTemplate', () => {
   const actual = resolveStringOrTemplate({
     template: '/search/issues?q=repo:${repo_nwo}+is:${issue_or_pr}',
@@ -22,5 +27,7 @@ test('resolveStringOrTemplate', () => {
   const expected = '/search/issues?q=repo:open-truss/open-truss+is:issue'
   expect(expected).toBe(actual)
 
-  expect(resolveStringOrTemplate(StringOrTemplate.parse(signal('hello')))).toBe('hello')
+  expect(resolveStringOrTemplate(StringOrTemplate.parse(signal('hello')))).toBe(
+    'hello',
+  )
 })
