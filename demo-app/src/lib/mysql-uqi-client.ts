f5fe6ee48e45d0d0709b9955124c4bf85335bb01
Jonathan Hoyt <jonmagic@gmail.com>
2024-05-23 09:56:49 -0700

    Update mysql-uqi-client to handle azure mysql user format requirement

--- demo-app/src/lib/mysql-uqi-client.ts
+++ demo-app/src/lib/mysql-uqi-client.ts
@@ -103,13 +103,22 @@ async function createMysqlUqiClient(config: MysqlConfig): Promise<UqiClient> {
     return asyncGenerator()
   }
 
+  let user = parsedUrl.username
+  const azureMySqlHostPattern = /^([a-z0-9-]+)\.mysql\.database\.azure\.com$/
+  const match = parsedUrl.hostname.match(azureMySqlHostPattern)
+
+  if (match) {
+    const host = match[1]
+    user = `${parsedUrl.username}@${host}`
+  }
+
   const client = mysql.createPool({
     host: parsedUrl.hostname,
     port: Number(parsedUrl.port),
     database: parsedUrl.pathname.slice(1),
     connectionLimit: 10, // will make up to this number of concurrent connections to mysql. subsequent requests are queued
     waitForConnections: true,
-    user: parsedUrl.username,
+    user,
     password: parsedUrl.password,
     socketPath: config.socketPath,
     rowsAsArray: true,
