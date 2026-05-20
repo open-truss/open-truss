ea600dfe20b6bf576ed47e12c9154efcb76eebe2
Keith McHugh <keith.mchugh@gmail.com>
2026-05-20 11:10:04 -0700

    Remove uqi from the demo app

--- demo-app/src/graphql/context.ts
+++ demo-app/src/graphql/context.ts
@@ -1,18 +1,11 @@
-import sources from '@/lib/uqi-sources'
-import { type UqiClient } from '@open-truss/uqi'
-
 export interface Context {
-  db: UqiClient
+  // database clients, etc.
 }
 
-const { config: dbConfig, createClient: createDbClient } = sources['mysql-demo']
-
 async function context(): Promise<Context> {
-  const db = await createDbClient(dbConfig)
-
   // Return anything needed in the global context like instantiated
   // database clients, etc.
-  return { db }
+  return {}
 }
 
 export default context
